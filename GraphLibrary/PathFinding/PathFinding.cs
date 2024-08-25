202344d8d7dfe3ac22f056e78036c2481e565d2d
Dominik Smola <dsmola9@gmail.com>
2024-08-25 20:20:25 +0200

    bellmanford alg

--- GraphLibrary/PathFinding/PathFinding.cs
+++ GraphLibrary/PathFinding/PathFinding.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.Resources;
+using System.Reflection.Metadata;
 using GraphLibrary.GraphRepresentation;
 using GraphLibrary.Utils;
 
@@ -8,11 +8,11 @@ namespace GraphLibrary.PathFinding;
 
 public static class PathFinding
 {
-    
-    public static List<Edge> DijkstraPathFinding(IGraphRepresentation graph,int startVertex, int endVertex)
+    public static List<Edge> DijkstraPathFinding(IGraphRepresentation graph, int startVertex, int endVertex)
     {
-        if(Utils.Utils.HasGraphNegativeEdge(graph))
-            throw new System.ArgumentException("Graph has negative edge");
+        if (Utils.Utils.HasGraphNegativeEdge(graph))
+            throw new ArgumentException("Graph has negative edge, try different algorithm");
+        
         var vertexQueue = new PriorityQueue<Vertex, int>();
         var vertexList = new List<Vertex>();
         foreach (var vertexId in graph.GetVertices())
@@ -22,9 +22,11 @@ public static class PathFinding
             {
                 vertex.Distance = 0;
             }
+
             vertexList.Insert(vertexId, vertex);
             vertexQueue.Enqueue(vertex, vertex.Distance);
         }
+
         while (vertexQueue.Count > 0)
         {
             var vertex = vertexQueue.Dequeue();
@@ -39,11 +41,62 @@ public static class PathFinding
                 }
             }
         }
+
         var path = new List<Edge>();
         var currentVertex = vertexList[endVertex];
         while (currentVertex.Previous != null)
         {
-            path.Insert(0, new Edge(currentVertex.Previous.Id, currentVertex.Id, currentVertex.Distance - currentVertex.Previous.Distance));
+            path.Insert(0,
+                new Edge(currentVertex.Previous.Id, currentVertex.Id,
+                    currentVertex.Distance - currentVertex.Previous.Distance));
+            currentVertex = currentVertex.Previous;
+        }
+
+        return path;
+    }
+
+    public static List<Edge> BellmanFordPathFinding(IGraphRepresentation graph, int startVertex, int endVertex)
+    {
+        var vertexList = new List<Vertex>();
+        foreach (var vertexId in graph.GetVertices())
+        {
+            var vertex = new Vertex(vertexId);
+            if (vertex.Id == startVertex)
+            {
+                vertex.Distance = 0;
+            }
+
+            vertexList.Insert(vertexId, vertex);
+        }
+
+        for (var i = 0; i < graph.GetVertices().Count - 1; i += 1)
+        {
+            foreach (var edges in graph.GetEdges())
+            {
+                var distance = vertexList[edges.StartVertex].Distance + edges.Weight;
+                if (distance < vertexList[edges.EndVertex].Distance)
+                {
+                    vertexList[edges.EndVertex].Distance = distance;
+                    vertexList[edges.EndVertex].Previous = vertexList[edges.StartVertex];
+                }
+            }
+        }
+
+        foreach (var edges in graph.GetEdges())
+        {
+            if (vertexList[edges.StartVertex].Distance + edges.Weight < vertexList[edges.EndVertex].Distance)
+            {
+                throw new ArgumentException("Graph has negative cycle");
+            }
+        }
+
+        var path = new List<Edge>();
+        var currentVertex = vertexList[endVertex];
+        while (currentVertex.Previous != null)
+        {
+            path.Insert(0,
+                new Edge(currentVertex.Previous.Id, currentVertex.Id,
+                    currentVertex.Distance - currentVertex.Previous.Distance));
             currentVertex = currentVertex.Previous;
         }
         return path;
--- GraphTDD/PathFindingTests.cs
+++ GraphTDD/PathFindingTests.cs
@@ -28,5 +28,53 @@ public class PathFindingTests
             length += edge.Weight;
         }
         Assert.Equal(5, length);
+    }   
+    
+    [Fact]
+    public void DijkstraNegativeEdge()
+    {
+        var graph = new Graph();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddEdge(0, 1, -1, true);
+        Assert.Throws<ArgumentException>(() => graph.PathFinding(0, 1, PathFinding.DijkstraPathFinding));
     }
+    
+    [Fact]
+    public void BellmanFord()
+    {
+        var graph = new Graph();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddEdge(0, 1, 1, true);
+        graph.AddEdge(0, 2, 1, true);
+        graph.AddEdge(0, 3, -1, true);
+        graph.AddEdge(1, 3, 3, true);
+        graph.AddEdge(1, 4, 10, true);
+        graph.AddEdge(2, 3, 3, true);
+        graph.AddEdge(3, 4, 2, true);
+        var path = graph.PathFinding(0, 4, PathFinding.BellmanFordPathFinding);
+        int length = 0;
+        foreach (var edge in path)
+        {
+            length += edge.Weight;
+        }
+        Assert.Equal(1, length);
+    }  
+    [Fact]
+    public void BellmanFordNegativeCycle()
+    {
+        var graph = new Graph();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddVertex();
+        graph.AddEdge(0, 1, -1);
+        graph.AddEdge(1, 2, 1);
+        graph.AddEdge(2, 0, 1);
+        var path = graph.PathFinding(0, 2, PathFinding.BellmanFordPathFinding);
+        
+    }   
 }
